32dc7effd472dee8d49b5e03ccc5a349eefc61ae
Monika <monika_streljajeva@hotmail.com>
2020-04-01 10:44:56 +0200

    cleaned up code

--- public/index.js
+++ public/index.js
@@ -10,20 +10,20 @@ function jsAvailable() {
 
 // show some styling only when js is activated
 function jsStyling() {
-    const group = document.querySelector(".group")
-    const saveButton = document.querySelector('#save');
-    const orderButton = document.querySelector('#order');
+    const group = document.querySelector('.group')
+    const saveButton = document.querySelector('#save')
+    const orderButton = document.querySelector('#order')
 
-    saveButton.style.display = 'none';
-    orderButton.style.display = 'none';
+    saveButton.style.display = 'none'
+    orderButton.style.display = 'none'
 
-    group.style.flexWrap = "nowrap";
+    group.style.flexWrap = 'nowrap'
 
 }
 
 //window.print() feature detection
 function printAvailable() {
-    const buttons = document.querySelector('.groupButton');
+    const buttons = document.querySelector('.groupButton')
 
     if (typeof window.print != 'undefined') {
         console.log('yeah, we kunnen printen!')
@@ -34,7 +34,7 @@ function printAvailable() {
         buttons.insertAdjacentHTML('beforeend', html)
 
         //print out page
-        document.querySelector('#printPage').addEventListener("click", function () {
+        document.querySelector('#printPage').addEventListener('click', function () {
             console.log('gaan we printen?')
             window.print()
         })
@@ -52,10 +52,10 @@ function printAvailable() {
 function storageAvailable() {
     try {
         var storage = window['localStorage'],
-            x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
+            x = '__storage_test__'
+        storage.setItem(x, x)
+        storage.removeItem(x)
+        return true
     } catch (e) {
         return e instanceof DOMException && (
                 // everything except Firefox
@@ -68,80 +68,80 @@ function storageAvailable() {
                 // Firefox
                 e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
             // acknowledge QuotaExceededError only if there's something already stored
-            storage.length !== 0;
+            storage.length !== 0
     }
 }
 
 if (storageAvailable('localStorage')) {
     console.log('Yippee! We can use localStorage awesomeness')
 
-    var i, radiobutton = document.querySelectorAll('input[type=radio]');
+    var i, radiobutton = document.querySelectorAll('input[type=radio]')
     var textinput = document.querySelector('#text')
     var textShirt = document.querySelector('#textShirt')
     var textTrui = document.querySelector('#textTrui')
 
     /*--- Put radio buttons in localstorage---*/
     for (i = 0; i < radiobutton.length; i++) {
-        radiobutton[i].addEventListener("click", function () {
+        radiobutton[i].addEventListener('click', function () {
             save()
-        });
+        })
     }
 
     function save() {
         for (i = 0; i < radiobutton.length; i++) {
-            localStorage.setItem(radiobutton[i].value, radiobutton[i].checked);
+            localStorage.setItem(radiobutton[i].value, radiobutton[i].checked)
         }
     }
 
     //load checked values of radio button
     for (i = 0; i < radiobutton.length; i++) {
-        radiobutton[i].checked = localStorage.getItem(radiobutton[i].value) === 'true' ? true : false;
+        radiobutton[i].checked = localStorage.getItem(radiobutton[i].value) === 'true' ? true : false
     }
 
     /*--- put text in local storage ---*/
-    textinput.addEventListener("input", function () {
-        localStorage.setItem("text", textinput.value)
+    textinput.addEventListener('input', function () {
+        localStorage.setItem('text', textinput.value)
     })
 
     // load text
-    textinput.value = localStorage.getItem("text", textinput.value)
-    textShirt.textContent = localStorage.getItem("text", textinput.value)
-    textTrui.textContent = localStorage.getItem("text", textinput.value)
+    textinput.value = localStorage.getItem('text', textinput.value)
+    textShirt.textContent = localStorage.getItem('text', textinput.value)
+    textTrui.textContent = localStorage.getItem('text', textinput.value)
 
 } else {
     console.log('Too bad, no localStorage for us')
 
     // when there's no localstorage, show the save button
-    const saveButton = document.querySelector('#save');
-    saveButton.style.display = 'block';
+    const saveButton = document.querySelector('#save')
+    saveButton.style.display = 'block'
 
 }
 
 // show selected shirt or trui
 function shirtPreview() {
-    const preview = document.querySelector(".preview")
-    const shirt = document.querySelector(".previewShirt")
-    const trui = document.querySelector(".previewTrui")
+    const preview = document.querySelector('.preview')
+    const shirt = document.querySelector('.previewShirt')
+    const trui = document.querySelector('.previewTrui')
 
     preview.style.display = 'initial'
     shirt.style.display = 'block'
 
-    document.querySelector('#shirt').addEventListener("click", function () {
+    document.querySelector('#shirt').addEventListener('click', function () {
         shirt.style.display = 'block'
         trui.style.display = 'none'
-    });
+    })
 
-    document.querySelector('#trui').addEventListener("click", function () {
+    document.querySelector('#trui').addEventListener('click', function () {
         trui.style.display = 'block'
         shirt.style.display = 'none'
-    });
+    })
 
     //check which type is selected and show the right preview
-    if (document.getElementById("shirt").checked == true) {
+    if (document.getElementById('shirt').checked == true) {
         shirt.style.display = 'block'
         trui.style.display = 'none'
         console.log('shirt')
-    } else if (document.getElementById("trui").checked == true) {
+    } else if (document.getElementById('trui').checked == true) {
         trui.style.display = 'block'
         shirt.style.display = 'none'
         console.log('trui')
@@ -150,82 +150,82 @@ function shirtPreview() {
 
 // Change color of shirt or trui
 function changeColor() {
-    const shirtPreview = document.querySelector('.previewShirt');
-    const truiPreview = document.querySelector('.previewTrui');
+    const shirtPreview = document.querySelector('.previewShirt')
+    const truiPreview = document.querySelector('.previewTrui')
 
     for (i = 0; i < document.querySelectorAll('input[name=stap2]').length; i++) {
-        document.querySelectorAll('input[name=stap2]')[i].addEventListener("click", function () {
+        document.querySelectorAll('input[name=stap2]')[i].addEventListener('click', function () {
             console.log(this.value)
 
-            shirtPreview.style.fill = this.value;
-            truiPreview.style.fill = this.value;
-        });
+            shirtPreview.style.fill = this.value
+            truiPreview.style.fill = this.value
+        })
     }
 
     //check which color is selected and show the right color in preview
 
     if (document.querySelector('#blauw').checked == true) {
-        shirtPreview.style.fill = '#4144f0';
-        truiPreview.style.fill = '#4144f0';
+        shirtPreview.style.fill = '#4144f0'
+        truiPreview.style.fill = '#4144f0'
     } else if (document.querySelector('#groen').checked == true) {
-        shirtPreview.style.fill = '#31e083';
-        truiPreview.style.fill = '#31e083';
+        shirtPreview.style.fill = '#31e083'
+        truiPreview.style.fill = '#31e083'
     } else if (document.querySelector('#geel').checked == true) {
-        shirtPreview.style.fill = '#f2f26b';
-        truiPreview.style.fill = '#f2f26b';
+        shirtPreview.style.fill = '#f2f26b'
+        truiPreview.style.fill = '#f2f26b'
     } else if (document.querySelector('#rood').checked == true) {
-        shirtPreview.style.fill = '#ed5858';
-        truiPreview.style.fill = '#ed5858';
+        shirtPreview.style.fill = '#ed5858'
+        truiPreview.style.fill = '#ed5858'
     } else if (document.querySelector('#oranje').checked == true) {
-        shirtPreview.style.fill = '#ed9458';
-        truiPreview.style.fill = '#ed9458';
+        shirtPreview.style.fill = '#ed9458'
+        truiPreview.style.fill = '#ed9458'
     } else if (document.querySelector('#roze').checked == true) {
-        shirtPreview.style.fill = '#f47aff';
-        truiPreview.style.fill = '#f47aff';
+        shirtPreview.style.fill = '#f47aff'
+        truiPreview.style.fill = '#f47aff'
     } else if (document.querySelector('#paars').checked == true) {
-        shirtPreview.style.fill = '#a963ff';
-        truiPreview.style.fill = '#a963ff';
+        shirtPreview.style.fill = '#a963ff'
+        truiPreview.style.fill = '#a963ff'
     } else if (document.querySelector('#zwart').checked == true) {
-        shirtPreview.style.fill = '#2e2e2e';
-        truiPreview.style.fill = '#2e2e2e';
+        shirtPreview.style.fill = '#2e2e2e'
+        truiPreview.style.fill = '#2e2e2e'
     } else if (document.querySelector('#wit').checked == true) {
-        shirtPreview.style.fill = 'white';
-        truiPreview.style.fill = 'white';
+        shirtPreview.style.fill = 'white'
+        truiPreview.style.fill = 'white'
     }
 }
 
 // Change text of shirt or trui
 function changeText() {
 
-    document.querySelector('#text').addEventListener("input", function () {
+    document.querySelector('#text').addEventListener('input', function () {
         var input = document.getElementById('text').value
 
-        document.querySelector('#textShirt').textContent = input;
-        document.querySelector('#textTrui').textContent = input;
+        document.querySelector('#textShirt').textContent = input
+        document.querySelector('#textTrui').textContent = input
     })
 }
 
 // Change text of shirt or trui
 function changeTextColor() {
-    const textShirt = document.querySelector('#textShirt');
-    const textTrui = document.querySelector('#textTrui');
+    const textShirt = document.querySelector('#textShirt')
+    const textTrui = document.querySelector('#textTrui')
 
     for (i = 0; i < document.querySelectorAll('input[name=stap4]').length; i++) {
-        document.querySelectorAll('input[name=stap4]')[i].addEventListener("click", function () {
+        document.querySelectorAll('input[name=stap4]')[i].addEventListener('click', function () {
             console.log(this.value)
 
-            textShirt.style.fill = this.value;
-            textTrui.style.fill = this.value;
-        });
+            textShirt.style.fill = this.value
+            textTrui.style.fill = this.value
+        })
     }
 
     //check which color is selected and show the right text color preview
     if (document.querySelector('#zwart1').checked == true) {
-        textShirt.style.fill = "#2e2e2e";
-        textTrui.style.fill = "#2e2e2e";
+        textShirt.style.fill = '#2e2e2e'
+        textTrui.style.fill = '#2e2e2e'
     } else if (document.querySelector('#wit1').checked == true) {
-        textShirt.style.fill = "#FFFFFF";
-        textTrui.style.fill = "#FFFFFF";
+        textShirt.style.fill = '#FFFFFF'
+        textTrui.style.fill = '#FFFFFF'
     }
 }
 
